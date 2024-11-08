f76a0928c42e5e7332a18aa995331be9ed7671ca
Mattis Hallberg <73825018+Mattman135@users.noreply.github.com>
2024-11-08 13:35:57 +0100

    Add files via upload

--- Analys2.R
+++ Analys2.R
@@ -8,10 +8,10 @@ library(gridExtra)
 library(lattice)
 library(patchwork)
 
-# Clear working environment and set seed.
+# Clear working environment.
 rm(list = ls())
 load("simulated_data.RData")
-
+df <- simulated_data
 #
 font_size <- 8 # Font size. May be changed to 8. 
 font_family = "sans" # Font family. "sans" (arial) or "serif" (times)
@@ -38,39 +38,41 @@ theme_update(axis.text = element_text(size = font_size, colour = "black", family
              text = element_text(size = font_size, colour = "black", family = font_family))
 update_geom_defaults("text", list(size = font_size / ggplot2:::.pt, family = font_family))
 
-#
-joint_lmm <- lmer(FEV1 ~ Setting + Age + Setting * Age + (-1 + as.factor(Setting) + Age:as.factor(Setting) | ID), data = df)
+# make a copy of df and add indices in order to easily extract
+df_copy <- df
+df_copy$Index <- c(1:nrow(df))
+N_FirstSubject <- nrow(df %>% filter(ID == 1))
+n_home <- nrow(df %>% filter(ID == 1, Setting == 1))
+n_hosp <- nrow(df %>% filter(ID == 1, Setting == 2))
+rowToSlice <- (df_copy %>% filter(ID == 1)) %>% slice(n()) # need the row to extract age and index
+rowToSlice
+indexToSlice <- rowToSlice$Index
+ageToSlice <- rowToSlice$Age
+df_prel <- df %>% slice(-indexToSlice)
 
-df_prel <- df %>% slice(-9)
+newdata <- select(rowToSlice, -c(Index, FEV1)) # deletes column: Index and FEV1
+
+# fitting models
 joint_lmm_2 <- lmer(FEV1 ~ Setting + Age + Setting * Age + (-1 + as.factor(Setting) + Age:as.factor(Setting) | ID), data = df_prel)
+joint_lmm <- lmer(FEV1 ~ Setting + Age + Setting * Age + (-1 + as.factor(Setting) + Age:as.factor(Setting) | ID), data = df)
+df_copy$predictions <- predict(joint_lmm)
+df_copy$predictions_LOOCV <- append(predict(joint_lmm_2), predict(joint_lmm_2, newdata = newdata), (indexToSlice-1))
 
-predict(joint_lmm_2)[1:8]
-predict(joint_lmm)[1:9]
-df$FEV1[1:9]
-(df %>% filter(ID == 1))$Age[9]
-new_data <- data.frame(ID = 1, Setting = 2, Measurement_nr = 4, Age = 17.11730)
-predict(joint_lmm_2, newdata = new_data)
-pred <- c(predict(joint_lmm_2)[1:8], predict(joint_lmm_2, newdata = new_data))
+# add
+df_firstOriginalData <- df %>% filter (ID == 1)
+df_firstModifiedData <- df %>% filter (ID == 1) # this will be same as df_firstOriginalData but the predicitons will be different
+df_firstOriginalData$predictions <- predict(joint_lmm)[1:N_FirstSubject]
+df_firstModifiedData$predictions <- c(predict(joint_lmm_2), predict(joint_lmm_2, newdata = newdata))[1:N_FirstSubject]
 
-(df %>% slice(9))$Age
+# extracting fixed effects
+fixedOriginalData <- fixef(joint_lmm)
+fixedModifiedData <- fixef(joint_lmm_2)
 
-age1 <- (df %>% filter(ID == 1, Setting == 1))$Age
-age2 <- (df %>% filter(ID == 1, Setting == 2))$Age
-age1and2 <- c(age1, age2)
-age1and2
-
-# fixed effects, population average trend
-fixed <- fixef(joint_lmm)
-fixed
-fixed2 <- fixef(joint_lmm_2)
-
-ggplot(data = df %>% filter(ID == 1), aes(x = Age, y = FEV1, color = as.factor(Setting))) + geom_point(size = 2) +
-  geom_line(data = df %>% filter(ID == 1, Setting == 1), aes(x = age1, y = (predict(joint_lmm)[1:5]) ), color = "blue") +
-  geom_line(data = df %>% filter(ID == 1, Setting == 2), aes(x = age2, y = (predict(joint_lmm)[6:9]) ), color = "red") +
-  geom_line(data = df %>% filter(ID == 1, Setting == 2), aes(x = age2, y = pred[6:9]), color = "green") +
-  #geom_line(aes(x = age1and2, y = fixed[1] + age1and2*fixed[3]), color = "black") + # fixed effect line, home
-  geom_line(aes(x = age1and2, y = fixed2[1] + age1and2*fixed2[3]), color = "black", linetype=2) + # fixed effect line on modified data, home
-  #geom_line(aes(x = age1and2, y = (fixed[1]+fixed[2]) + age1and2*(fixed[3]+fixed[4])), color = "black") + ## fixed effect line, hospital
-  geom_line(aes(x = age1and2, y = (fixed2[1]+fixed2[2]) + age1and2*(fixed2[3]+fixed2[4])), color = "black", linetype=2) + # fixed effect line, hospital
-  labs(title = "Data visualization of the first individual", color = "Setting", subtitle = "Dashed black lines are the fixed effect line's of home and hospital", 
-       caption = "Blue line: prediction of home spirometry, Read line: prediction of hospital spirometry, Green line: prediction of hospital but model is trained on modified data")
+ggplot(data = df %>% filter(ID == 1), aes(x = Age, y = FEV1, color = as.factor(Setting))) + geom_point(size = 2) + 
+  geom_line(data = df_copy %>% filter(ID == 1, Setting == 2), aes(x = Age, y = predictions), color = "blue") +
+  geom_line(data = df_copy %>% filter(ID == 1, Setting == 2), aes(x = Age, y = predictions_LOOCV), color = "red") +
+  geom_line(aes(x = Age, y = (fixef(joint_lmm)[1]+fixef(joint_lmm)[2] + (fixef(joint_lmm)[4])*Age )), color = "black") +
+  geom_line(aes(x = Age, y = (fixef(joint_lmm_2)[1]+fixef(joint_lmm_2)[2] + (fixef(joint_lmm_2)[4])*Age )), color = "black", linetype=2) #+
+  #labs(title = "Illustration first subject", color = "Setting", 
+       #subtitle = "Lines; Blue: Predictions on hospital data on full data, Red: predictions on hospital LOOCV, 
+                          #Black: Fixed effect line on full data, Black dashed: Fixed effect line LOOCV")
